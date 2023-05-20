0b7da8d5a23cb98c51004aae1a89be98deef6fb8
amith <cham@post.bgu.ac.il>
2023-05-20 21:18:27 +0300

    dasdasdas

--- tests.py
+++ tests.py
@@ -5,7 +5,8 @@ from main import calculate
 class TestApplication:
     def test_negative_one_team_with_unavailable_name(self):
         res = calculate(team1='Chelsea', team2='InvalidTeam', algorithm='RFC')
-        assertEquals(res, None, "should return None when one team is invalid")
+        self.assertEquals(
+            res, None, "should return None when one team is invalid")
 
     def test_positive_1(self):
         pass
