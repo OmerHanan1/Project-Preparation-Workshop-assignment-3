25a109059ae26cc100702f6642563f46935a6711
Omer Hanan <omer.hanan@gmail.com>
2023-05-20 22:20:53 +0300

    Update assert to Equal without s

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ from main import calculate
 class TestApplication(unittest.TestCase):
     def test_negative_one_team_with_unavailable_name(self):
         res = calculate(team1='Chelsea', team2='InvalidTeam', algorithm='RFC')
-        self.assertEquals(
+        self.assertEqual(
             res, None, "should return None when one team is invalid")
 
     def test_positive_1(self):
