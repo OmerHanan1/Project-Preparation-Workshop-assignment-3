7be05664a9247e8fe260b9fa115e237c3562b7de
amith <cham@post.bgu.ac.il>
2023-05-20 20:08:09 +0300

    added tests

---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestApplication:
+    def test_negative_one_team_with_anavailable_name(self):
+        pass
+
+    def test_positive_1(self):
+        pass
+
+    def test_positive_2(self):
+        pass
+
+
+# Run the tests
+if __name__ == '__main__':
+    unittest.main()
