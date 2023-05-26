3ca9ea168e98257be9b7d4332f379e2e486a728a
amith <cham@post.bgu.ac.il>
2023-05-26 18:17:05 +0300

    shavou tov

--- tests.py
+++ tests.py
@@ -5,27 +5,47 @@ import pandas as pd
 
 class TestApplication(unittest.TestCase):
     def test_negative_predication_one_team_with_bad_name(self):
+        """
+        test case: try to predict two teams game score but one team does not exist.
+        input: team1='Chelsea', team2='InvalidTeam', match_date="2015-09-09", algorithm='RFC'
+        output: should throw exception
+        """
         with self.assertRaises(Exception):
             res = prediction(team1='Chelsea', team2='InvalidTeam',
                              match_date="2015-09-09", algorithm='RFC')
 
     def test_positive_prediction_teams_with_good_names(self):
+        """
+        test case: try to predict two teams game score with valid.
+        input: team1='Chelsea', team2='Liverpool', match_date='2015-05-10', algorithm='RFC'
+        output: should return a tuple with ('Chelsea wins', 'Draw')
+        """
         res = prediction(team1='Chelsea', team2='Liverpool',
                          match_date='2015-05-10', algorithm='RFC')
         self.assertEqual(
             res, ('Chelsea wins', 'Draw'), "should return ('Chelsea wins', 'Draw')")
 
     def test_negative_getRowFromData_bad_teams_should_raise_exception(self):
+        """
+        test case: try to extract row from dataset with bad input.
+        input: team1='Chelsea', team2='falafel', match_date='2015-05-01'
+        output: should throw KeyError
+        """
         with self.assertRaises(KeyError):
             res = getRowFromData(team1='Chelsea', team2='falafel',
                                  match_date='2015-05-01')
 
     def test_positive_getRowFromData_return_type(self):
+        """
+        test case: try to extract row teams game score with valid input.
+        input: team1='Chelsea', team2='Liverpool', match_date='2015-05-10'
+        output: should return a variable of type pd.core.frame.DataFrame
+        """
         res = getRowFromData(team1='Chelsea', team2='Liverpool',
                              match_date='2015-05-10')
         res = res[0]
         self.assertEqual(type(res), pd.core.frame.DataFrame,
-                         "should return 'pd.core.series.Series'")
+                         "should return 'pd.core.frame.DataFrame'")
 
 
 # Run the tests
