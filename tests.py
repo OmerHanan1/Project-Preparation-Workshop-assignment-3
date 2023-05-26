e9a4fa441fa937f423edace70fb2a446156315cd
amith <cham@post.bgu.ac.il>
2023-05-26 15:58:56 +0300

    dds

--- tests.py
+++ tests.py
@@ -3,13 +3,19 @@ from main import calculate
 
 
 class TestApplication(unittest.TestCase):
-    def test_negative_one_team_with_unavailable_name(self):
-        res = calculate(team1='Chelsea', team2='InvalidTeam', algorithm='RFC')
+    def test_negative_calculate_one_team_with_unavailable_name(self):
+        res = calculate(team1='Chelsea', team2='InvalidTeam',
+                        match_date="2015-09-09", algorithm='RFC')
+        print(res)
         self.assertEqual(
             res, None, "should return None when one team is invalid")
 
-    def test_positive_1(self):
-        pass
+    def test_positive_calculate_one_team_with_unavailable_name(self):
+        res = calculate(team1='Chelsea', team2='Liverpool',
+                        match_date='2015-06-07', algorithm='RFC')
+        print(res)
+        self.assertEqual(
+            res, None, "should return None when one team is invalid")
 
     def test_positive_2(self):
         pass
